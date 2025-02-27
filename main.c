48ba79be85a75a17f6a9bf1a97c10688b147d533
David Neškrabal <davidneskrabal@Davids-MacBook-Air.local>
2025-02-27 19:00:31 +0000

    comments added

--- main.c
+++ main.c
@@ -6,15 +6,9 @@
 #include "my_library.h"
 
 /************ TO-DO's *************
- * add initial welcome and option for test or own values (mentioned in Assignment 2 - Specification (2025).pdf
- * check if input from user regarding months is within <1,12>
- * consider changing print_month to be more general
- *     - ie. return_type char so that it can be used within printf
- *     - rename
- * do input month function
- * A) 2) from Assignment 2 - Specification (2025).pdf
- * A) 3) from Assignment 2 - Specification (2025).pdf
- * A) 4) from Assignment 2 - Specification (2025).pdf
+
+- Make a recursive function that creates a chain of dependencies and
+  checks for circular dependency
  **********************************/
 
 int main(void)
@@ -33,19 +27,26 @@ int main(void)
      ************************************************/
     initial_action(tasks, &num_tasks);
 
-    display_Gantt_diagram(tasks, num_tasks);
 
-
-    puts("If you wish to edit the Gantt please type \"edit\" "
-    "/ If you wish to run a test, type \"test\" "
-    "or to exit, type \"quit\" and then press enter to execute your option.");
-    scanf("%s", response);
-
-    if (strcmp("edit", response) == 0)
+    do
     {
-        action_edit(tasks, &num_tasks);
         display_Gantt_diagram(tasks, num_tasks);
-    }
+
+        puts("If you wish to edit the Gantt please type \"edit\" "
+        "/ If you wish to run a test, type \"test\" "
+        "or to exit, type \"quit\" and then press enter to execute your option.");
+        scanf("%s", response);
+
+        if (strcmp("edit", response) == 0)
+        {
+            action_edit(tasks, &num_tasks);
+        }
+        else if (strcmp("test", response) == 0)
+        {
+            // add dependency function here
+        }
+    } while (strcmp("quit", response) != 0);
+
 
 
 
--- my_library.c
+++ my_library.c
@@ -36,46 +36,60 @@ void print_cell(char *string, int width)
 
 void print_char(char a)
 {
-    for (int k = 0; k < (12 * 11) + 21 + 13; k++) printf("%c", a);
+    // print character (size of big header cell, month header cell * 12, dependencies header cell) times
+    for (int k = 0; k < (12 * 11) + 21 + 13; k++)
+    {
+        printf("%c", a);
+    }
     printf("\n");
 }
 
 void print_header(void)
 {
-    print_char('_');
-    printf("%20s", "");
-    char columns[][13] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "Dependencies"};
+    print_char('_'); // print separation line
+    printf("%20s", ""); // print first cell (empty space)
+    // initialize months array of strings
+    char columns[][13] = { "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December", "Dependencies"};
+
+    // print column header for each month
     for (int i = 0; i < 13; i++)
     {
         print_cell(columns[i],10);
     }
     printf("|\n");
-    print_char('-');
+    print_char('-'); // print separation line
 }
 
 void get_months(unsigned int * start_month, unsigned int * end_month)
 {
+    // keep prompting user to get starting month until valid input is inserted (1-12)
     do
     {
         puts("Please enter the starting month (1-12):");
-        scanf("%u", start_month);
+        scanf("%u", start_month); // store number in start_month
 
     } while (*start_month < 1 || *start_month > 12);
+
+    //  keep prompting user to get starting month until valid
+    //  input is inserted (1-12) and more or equal to start_month
     do
     {
         puts("Please enter the ending month (1-12):");
         scanf("%u", end_month);
 
+        // inform user of incorrect input
         if (*end_month < *start_month)
         {
             puts("End month cannot be before start month. Try again.");
         }
+
     } while (*end_month < *start_month || *end_month > 12);
 }
 
 void swap(unsigned int *a, unsigned int *b)
 {
-    unsigned int tmp = *a;
+    unsigned int tmp = *a; // store a in tmp to prevent overwriting data
     *a = *b;
     *b = tmp;
 }
@@ -103,61 +117,75 @@ void get_dependencies(unsigned int * num_dependency, unsigned int dependency_id[
     scanf("%u", num_dependency);
     if (*num_dependency != 0) // if there are any dependencies, ask to which tasks
     {
+        // prompt user to get the task numbers the current task is dependent on
         for (int i = 0; i < *num_dependency; i++)
         {
             printf("Please enter dependent task (%d/%u):", i+1, *num_dependency);
-            scanf("%u", &dependency_id[i]);
+            scanf("%u", &dependency_id[i]); // store task id's in an array
         }
     }
-    bubble_sort(dependency_id, *num_dependency);
+    bubble_sort(dependency_id, *num_dependency); // sort id's
 }
 
 void get_Task_data(Task tasks[], unsigned int * num_tasks)
 {
     int i;
 
+    // prompt user to insert the number of tasks
     puts("How many tasks would you like to add? (1-10)");
-    scanf("%u", num_tasks);
+    scanf("%u", num_tasks); // store num of tasks in num_tasks
     // prompt user to input details about each task
     for (i = 0; i < *num_tasks; i++)
     {
         // Prompts user to Enter task name
         puts("Please enter the task name:");
-        scanf("%s", tasks[i].name);
+        scanf("%s", tasks[i].name); // store name in char array
 
+        // call function to get months input to the current task from user
         get_months(&tasks[i].start_month, &tasks[i].end_month);
 
+        // call function to get dependency input to the current task from user
         get_dependencies(&tasks[i].dependencies, tasks[i].dependency_id);
     }
 }
 
 void display_Gantt_diagram(Task tasks[], int num_tasks)
 {
-    int i, j;
+    int i, j; // initialize loop counter variables
+
+    // prints the gantt diagram's header
     print_header();
-    // ************************** TEST ***********************
+
+    // print row for each task, depicting in which months is the task being worked on
     for (i = 0; i < num_tasks; i++)
     {
         printf("%-20s", tasks[i].name); // Print task name aligned
 
+        // print a cell for each month filling the cell if the task
+        // is being worked on that month
         for (j = 1; j <= 12; j++)
         {
+            // check if j which represents the current month is
+            // within range of start and end month
             if (j >= tasks[i].start_month && j <= tasks[i].end_month)
             {
-                print_cell("XXX", 10);
+                print_cell("XXX", 10); // fill the cell with x's
             }
+            // keep the cell empty
             else
             {
                 print_cell("", 10);
             }
         }
-        printf("|");
+        printf("|"); // box of the last cell
+
+        // prints the dependenies
         for (j = 0; j < tasks[i].dependencies; j++)
         {
             printf(" %u", tasks[i].dependency_id[j]);
         }
         printf("\n");
-        print_char('-');
+        print_char('-'); // prints separations line
     }
 }
 
@@ -165,24 +193,30 @@ void initial_action(Task tasks[], unsigned int *num_tasks)
 {
     char response[20];
     bool finished = false;
+
+    // display welcome message
     display_menu();
 
+    // continue getting input from user until valid input is inserted
     do
     {
-        scanf("%s", response);
+        scanf("%s", response); // get user input
+
+        // check if user wants to work with testing values
         if (strcmp(response, "test") == 0)
         {
             initialize_test_values(tasks, num_tasks); // inputs test values to struct tasks
-            finished = true;
+            finished = true; // assign true to finished to exit the do-while loop
         }
         else if (strcmp(response, "create") == 0)
         {
             // prompts user get details about each task
             get_Task_data(tasks, num_tasks);
-            finished = true;
+            finished = true; // assign true to finished to exit the do-while loop
         }
         else
         {
+            // inform user that wrong input has been given
             puts("Wrong input - Enter \"test\" or \"create\"");
         }
     } while (!finished);
