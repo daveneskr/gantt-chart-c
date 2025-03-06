f29ed58cecf007b5f4419c0a5286c765bb2b125a
David Neškrabal <davidneskrabal@Davids-MacBook-Air.local>
2025-03-06 14:13:54 +0000

    main commented

--- main.c
+++ main.c
@@ -13,7 +13,8 @@ int main(void)
     /******************** SECTION 0 *****************
      * define and initialize variables used throughtout the main
      ************************************************/
-    Task tasks[MAX_TASKS];
+
+    Task tasks[MAX_TASKS]; // initialize tge task array
     unsigned int num_tasks; // number of tasks
     char response[20];
 
@@ -23,11 +24,23 @@ int main(void)
      * testing for valid input
      ************************************************/
 
-
+    // Sets up the program at the beginning, get values for Tasks
+    // 1. Ask user where to get values for tasks
+    //   a) user input - prompts user to get details about each task
+    //   b) test values - inputs test values to struct tasks
+    // 2. Stores values in Task tasks
     initial_action(tasks, &num_tasks);
 
+    // Displays the Gantt diagram of the tasks
     display_Gantt_diagram(tasks, num_tasks);
 
+    /******************** SECTION 2 *****************
+    * menu-like state, where user is offer actions
+    * editing, tasks, checking dependencies
+    * quiting the program
+    ************************************************/
+
+    // continue offer action option to user until the program is quit
     do
     {
         puts("\nIf you wish to edit the Gantt please type \"edit\" "
@@ -35,21 +48,27 @@ int main(void)
         "or to exit, type \"quit\" and then press enter to execute your option.");
         scanf("%s", response);
 
+        // if user chooses to edit
         if (strcmp("edit", response) == 0)
         {
+            // prompts user to edit a task
             action_edit(tasks, &num_tasks);
+            // clears the previous Gantt diagram
             clear_screen();
+            // displays Gantt with edited task
             display_Gantt_diagram(tasks, num_tasks);
         }
         else if (strcmp("test", response) == 0)
         {
+            // ask user which task they would like to test
             puts("Enter the name of the task you would like to test:");
             scanf("%s", response);
+            // look through tasks to find one mathcing to response
             for (unsigned int i = 0; i < num_tasks; i++)
             {
                 if (strcmp(tasks[i].name, response) == 0)
                 {
-                    // Print dependency chain
+                    // print dependency chain
                     printf("\nDependency Chain:\n");
                     int visited[MAX_TASKS] = {0}; // track visited tasks
                     print_dependency_chain(tasks, tasks[i].id, visited);
@@ -58,8 +77,15 @@ int main(void)
         }
     } while (strcmp("quit", response) != 0);
 
+    /******************** SECTION 3 *****************
+    * final section, where the screen is cleared
+    * and ascii art is displayed
+    ************************************************/
+
+    // clear screen from program output
     clear_screen();
 
+    // display ascii art
     print_ascii_art();
 
     return(0);
